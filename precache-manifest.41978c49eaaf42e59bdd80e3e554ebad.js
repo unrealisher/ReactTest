b9ebb0f5ce9006f9b995a167747e54f68170c09d
unrealisher <unrealmpa3@mail.ru>
2019-01-19 13:54:33 +0300

    Updates

---
+++ precache-manifest.41978c49eaaf42e59bdd80e3e554ebad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b34b72a80b4f257bdccb",
+    "url": "/ReactTest/todo/static/js/runtime~main.b34b72a8.js"
+  },
+  {
+    "revision": "3150ada5eb61125a3be6",
+    "url": "/ReactTest/todo/static/js/main.3150ada5.chunk.js"
+  },
+  {
+    "revision": "f11e32fb227b6aa7afc0",
+    "url": "/ReactTest/todo/static/js/1.f11e32fb.chunk.js"
+  },
+  {
+    "revision": "3150ada5eb61125a3be6",
+    "url": "/ReactTest/todo/static/css/main.23e92fe2.chunk.css"
+  },
+  {
+    "revision": "dc08496e7d1227ef8436f8afc2d10b6d",
+    "url": "/ReactTest/todo/index.html"
+  }
+];
